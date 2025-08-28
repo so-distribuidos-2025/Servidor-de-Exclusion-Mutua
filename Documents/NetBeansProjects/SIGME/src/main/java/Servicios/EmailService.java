ca592558822448d1edcc59a1a08300d57395e5af
Juan Maximiliano Gomez <109885142+RedencionMc22@users.noreply.github.com>
2025-08-28 18:57:18 -0300

    Delete Documents directory

--- Documents/NetBeansProjects/SIGME/src/main/java/Servicios/EmailService.java
+++
@@ -1,8 +0,0 @@
-package Servicios;
-
-public class EmailService {
-    public void enviarMail(String destinatario, String asunto, String mensaje) {
-        System.out.println("Enviando mail a " + destinatario + " con asunto: " + asunto);
-        // Lógica real de envío en versión web
-    }
-}
